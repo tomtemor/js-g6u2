8ddd0e6d694c3c64455d072a4e4a2fad685c38c5
Ronja Ö <tomoe_cchi@hotmail.com>
2021-12-12 12:05:48 +0100

    Småfixar! Nästan klara!

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-/*
-index.js
-
-Gruppuppgift del 2, hämta från API
+/* Gruppuppgift del 2, hämta från API.
 SYNED21JON Grupp 6
+
 Medlemmar:
 Christer Klasson 
 Ida Gustafsson 
@@ -11,29 +9,20 @@ Marianne Nordlund
 Matti Heinonen 
 Ronja Österback
 
-Instruktioner:
-Del 2: Hämta data från API
-Gör en applikation där användaren hämtar data från Ghibli-API:et
-https://ghibliapi.herokuapp.com/. Den ska vara interaktiv på något sätt, t.ex. kan användaren
-trycka på en knapp för att information om filmer ska visas (utan att hela sidan laddas om)
-eller browsa genom en lista med länkar till olika filmer, men det behöver inte vara någon
-särskilt omfattande applikation. Det ska vara någorlunda angenämt för en människa att läsa,
-så du behöver använda JSON.parse och förmodligen loopa igenom det returnerade
-JavaScript-objektet innan du skriver ut på webbsidan.
-
 Kommentarer:
-Vi har gjort en applikation som hämtar data från Ghibli enligt instruktioner. resultatet(filmerna) listas och numreras och presenteras
-med namn, orignalnamn och år samt en bild. Användaren kan trycka på en knapp för att se en video/trailer av vardera film.
-All funktionalitet ligger i ett klient-javascript client.js 
-
-*/
+Vi har gjort en applikation som hämtar data från Ghibli enligt instruktioner.
+Resultatet (filmerna) listas, numreras och presenteras med namn, orignalnamn
+och år samt en bild. Användaren kan trycka på en knapp för att se en video/trailer
+av vardera film. All funktionalitet ligger i klientskriptet client.js. */
 
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");  // visa html-sida
-});
-app.listen(3000); // lyssna på port 3000
-console.log("Kör servern på localhost:3000");
-app.use(express.static("public")); // publik mapp
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "public"))); // Publik mapp.
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html"); // Visa html-sida.
+});
+
+app.listen(3000, () => console.log("Kör servern på localhost:3000")); // Lyssna på port 3000.
--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,143 +1,187 @@
-/*
-Gruppuppgift del 2, hämta från API
-SYNED21JON Grupp 6
-LET Medlemmar = [
-{Christer Klasson}, 
-{Ida Gustafsson}, 
-{Julia Magnusson}, 
-{Marianne Nordlund}, 
-{Matti Heinonen}, 
-{Ronja Österback}
-];
-*/
+window.onload = function () {
+  // Hämtar data från API.
+  forfragan = new XMLHttpRequest();
 
+  forfragan.open("GET", "https://ghibliapi.herokuapp.com/films");
 
-window.onload = function() {
-   
-  
-    // Hämtar data från API
-    forfragan = new XMLHttpRequest();
-    forfragan.open("GET", "https://ghibliapi.herokuapp.com/films");
-    forfragan.onload = function() {
-        console.log("Svar från servern");
-        let filmer = JSON.parse(this.response);
-       // console.log(filmer);
-        let lista = "";
-        let trailer_trailer = "";
-        let trailers =[  // array med lista av objekt med filmerna
-            {title:'Castle in the Sky', 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/8ykEy-yPBFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:'Grave of the Fireflies', 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/4vPeTSRd580" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:'My Neighbor Totoro', 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/92a7Hj0ijLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Kiki's Delivery Service", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/4bG17OYs-GA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Only Yesterday", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/x0ZrjocXVJ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Porco Rosso", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/awEC-aLDzjs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Pom Poko", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/_7cowIHjCD4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Whisper of the Heart", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/0pVkiod6V0U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Princess Mononoke", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/4OiMOHRDs14" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"My Neighbors the Yamadas", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/1C9ujuCPlnY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Spirited Away", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/ByXuk9QqQkk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"The Cat Returns", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/Gp-H_YOcYTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Howl's Moving Castle", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/iwROgK94zcM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Tales from Earthsea", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/8hxYx3Jq3kI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Ponyo", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/CsR3KVgBzSM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"Arrietty", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/9CtIXPhPo0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"From Up on Poppy Hill", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/9nzpk_Br6yo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"The Wind Rises", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/2QFBZgAZx7g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"The Tale of the Princess Kaguya", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/W71mtorCZDw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"When Marnie Was There", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/jjmrxqcQdYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            },
-            {title:"The Red Turtle", 
-            trailer:'<iframe width="560" height="315" src="https://www.youtube.com/embed/Sw7BggqBpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-            }
-        ];
-       // siffra är bara en räknare för numrering av filmerna i listan
-       let siffra = 1;
-       lista += "<table border='0'>";
-        for (let i = 0; i < filmer.length; i++) { // loopar igenom alla filmer i filmer och sparar i lista
-            
-            lista += "<tr><td class = 'title' colspan=3>" + siffra +". " + filmer[i].title + "</b> | " + filmer[i].release_date + " | " + filmer[i].original_title + "</div></td></tr>";
-            lista += "<tr><td style='border:none; width: 20%'> <div class='tab1'></div></td><td style='border:none; width: 40%;'></td><td style='border:none;'></td></tr>";
-            lista += "<tr><td> <img class='image' src=' " + filmer[i].image + "'></td>";
-            lista += "<td style='vertical-align: top;'> <button onclick='showVID(" + i + ")'>Visa/göm trailer</button> <div id='" + i + "'>";
-             
-            if ( trailers[i] === undefined ){
-                trailer_trailer = "";
-            } else {
-            trailer_trailer = trailers[i].trailer;
-            
-        }
-             
-            lista += trailer_trailer; // lägger till youtube-iframe-koden för trailern
-           
-            lista += "</div></td><td>" + filmer[i].description +  "</td></tr>";
-            siffra++;
-          }
-          lista += "</table>";
+  forfragan.onload = function () {
+    console.log("Svar från servern");
 
-            output1.innerHTML=lista;  // skickar innehållet(html-koden) i 'lista' till en p med id output1 i index.html
+    let filmer = JSON.parse(this.response);
+    let lista = "";
+    let trailer_trailer = "";
+    // Array med lista av filmerna som objekt.
+    let trailers = [
+      {
+        title: "Castle in the Sky",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/8ykEy-yPBFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Grave of the Fireflies",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/4vPeTSRd580" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "My Neighbor Totoro",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/92a7Hj0ijLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Kiki's Delivery Service",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/4bG17OYs-GA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Only Yesterday",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/x0ZrjocXVJ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Porco Rosso",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/awEC-aLDzjs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Pom Poko",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/_7cowIHjCD4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Whisper of the Heart",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/0pVkiod6V0U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Princess Mononoke",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/4OiMOHRDs14" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "My Neighbors the Yamadas",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/1C9ujuCPlnY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Spirited Away",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/ByXuk9QqQkk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "The Cat Returns",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/Gp-H_YOcYTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Howl's Moving Castle",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/iwROgK94zcM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Tales from Earthsea",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/8hxYx3Jq3kI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Ponyo",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/CsR3KVgBzSM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "Arrietty",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/9CtIXPhPo0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "From Up on Poppy Hill",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/9nzpk_Br6yo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "The Wind Rises",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/2QFBZgAZx7g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "The Tale of the Princess Kaguya",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/W71mtorCZDw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "When Marnie Was There",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/jjmrxqcQdYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+      {
+        title: "The Red Turtle",
+        trailer:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/Sw7BggqBpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      },
+    ];
 
-          // loop som gömmer alla video divs från början
-            for (let i = 0; i < filmer.length; i++) { 
-            var e = document.getElementById(i);
-            if(e.offsetWidth > 0 || e.offsetHeight > 0){
-              e.style.display = 'none';
-            }}
-            
-          
-    };
-    
-    
-    forfragan.send();
+    // Siffra är bara en räknare för numrering av filmerna i listan.
+    let siffra = 1;
+    lista += "<table border='0'>";
 
-}
+    for (let i = 0; i < filmer.length; i++) {
+      // Loopar igenom alla filmer i filmer och sparar i lista.
+      lista +=
+        "<tr><td class = 'title' colspan=3>" +
+        siffra +
+        ". " +
+        filmer[i].title +
+        "</b> | " +
+        filmer[i].release_date +
+        " | " +
+        filmer[i].original_title +
+        "</div></td></tr>";
+      lista +=
+        "<tr><td style='border:none; width: 20%'> <div class='tab1'></div></td><td style='border:none; width: 40%;'></td><td style='border:none;'></td></tr>";
+      lista +=
+        "<tr><td> <img class='image' src=' " + filmer[i].image + "'></td>";
+      lista +=
+        "<td style='vertical-align: top;'> <button onclick='showVID(" +
+        i +
+        ")'>Visa/göm trailer</button> <div id='" +
+        i +
+        "'>";
 
+      if (trailers[i] === undefined) {
+        trailer_trailer = "";
+      } else {
+        trailer_trailer = trailers[i].trailer;
+      }
 
+      lista += trailer_trailer; // Lägger till youtube-iframe-koden för trailern.
 
-function showVID(i) { //funktion för att gömma/visa filmerna anropas med onclick på knapparna
- 
-  var x = document.getElementById(i);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+      lista += "</div></td><td>" + filmer[i].description + "</td></tr>";
+      siffra++;
+    }
+
+    lista += "</table>";
+
+    output1.innerHTML = lista; // Skickar innehållet (html-koden) i 'lista' till en <p> med id output1 i index.html.
+
+    // Loop som gömmer alla video divs från början.
+    for (let i = 0; i < filmer.length; i++) {
+      let e = document.getElementById(i);
+
+      if (e.offsetWidth > 0 || e.offsetHeight > 0) {
+        e.style.display = "none";
+      }
+    }
+  };
+
+  forfragan.send();
+
+  // Funktion för att gömma/visa filmerna anropas med onclick på knapparna
+  function showVID(i) {
+    let x = document.getElementById(i);
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
-}
+};
